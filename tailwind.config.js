3cab0972a4f901ca0a9727515d1953cfd6908d5f
Jordan Place <tapman90@gmail.com>
2020-06-24 09:54:20 -0700

    responsive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   theme: {
     fontSize: {
-      "header-xs": "1.25vw",
-      "header-sm": "2.5vw",
-      "header-md": "3vw",
-      "header-lg": "4.5vw",
-      "section-header": "3.5vw",
-      "section-detail": "1.75vw",
+      "header-xs": "2.5vmin",
+      "header-sm": "5vmin",
+      "header-md": "6vmin",
+      "header-lg": "9vmin",
+      "section-header": "7vmin",
+      "section-detail": "3.5vmin",
+      "md/header-xs": "1.25vw",
+      "md/header-sm": "2.5vw",
+      "md/header-md": "3vw",
+      "md/header-lg": "4.5vw",
+      "md/section-header": "3.5vw",
+      "md/section-detail": "1.75vw",
     },
     extend: {
       colors: {
@@ -23,14 +29,19 @@ module.exports = {
         nunito: ["Nunito", "sans-serif"],
       },
       spacing: {
-        sm: "1.25vw",
-        base: "1.75vw",
-        md: "2vw",
-        outer: "3vw",
+        "md/sm": "1.25vw",
+        "md/base": "1.75vw",
+        "md/md": "2vw",
+        "md/outer": "3vw",
+        sm: "2.5vmin",
+        base: "3.5vmin",
+        md: "4vmin",
+        outer: "6vmin",
       },
       lineHeight: {
         bound: "1.125",
       },
+      padding: { "1/3": "33.333333%" },
     },
   },
   variants: {},
