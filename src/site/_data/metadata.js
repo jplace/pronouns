f0175a3ff27e43e156995da616db2f0db57f5724
Jordan Place <tapman90@gmail.com>
2020-09-20 09:44:12 -0700

    remove trailing slash

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "https://pronouns.work/", // use this only when absolute URLs are required
+  url: "https://pronouns.work", // use this only when absolute URLs are required
   defaultDescription:
     "A guide to handling instances of misgendering with grace",
 };
